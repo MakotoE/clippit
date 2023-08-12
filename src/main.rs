7af4d846117e2521b2deba96a0449eaa0aef39d3
Makoto Emura <makoto@emurasoft.com>
2023-08-11 17:50:46 -0700

    Fix clippy command

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,27 @@ use anyhow::Result;
 use clippit::{output};
 use std::process::Command;
 
+/// Use -v to see the `cargo clippy` command and output.
 fn main() -> Result<()> {
-    let is_verbose = args().any(|arg| {arg == "-v" || arg == "--v"});
+    let mut args: Vec<String> = args().skip(1).collect();
+    args.insert(0, "clippy".to_string());
+
+    let is_verbose = args.iter().any(|arg| {arg == "-v" || arg == "--verbose"});
 
     let mut command = Command::new("cargo");
-    command.args(args());
+
+    command.args(args);
 
     let clippy_output = command.output()?;
     let clippy_string = std::str::from_utf8(&*clippy_output.stderr)?;
 
     if is_verbose {
-        println!("clippy command: {:?}", command);
-        println!("clippy output: {clippy_string}");
+        eprintln!("clippy command: {:?}", command);
+        eprintln!("clippy output: {clippy_string}");
     }
 
 
     output(clippy_string, &mut std::io::stderr())?;
 
-    Ok(())
+    std::process::exit(clippy_output.status.code().unwrap_or(0));
 }
